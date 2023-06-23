f69a2f50fc4a5d0cd2742c33ddfc4e9e839c63c0
DGorniaczyk <dawidoo12345678@gmail.com>
2023-06-23 11:28:34 +0200

    Added the code for section_app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WelcomeHeaderComponent } from './components/welcome-header/welcome-header.component';
+import { SectionListComponent } from './components/section-list/section-list.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomeHeaderComponent,
+    SectionListComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
